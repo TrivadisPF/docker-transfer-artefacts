f8390ba79a17006b1a06a2b6235ab85098cd27b3
Guido Schmutz <guido.schmutz@trivadis.com>
2023-12-11 15:58:08 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ FROM alpine:latest
 COPY jdbc-drivers /libs/jdbc-drivers
 COPY ksqldb-udfs /libs/ksqldb-udfs
 COPY nifi-nars /libs/nifi-nars
+COPY config /config
 
 # copy the generator script and make it executable
 COPY run.sh /usr/local/bin/
--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-mkdir -p /data-transfer/libs
-cp -R /libs/* /data-transfer/libs
+mkdir -p /data-transfer/artefacts/libs
+cp -R /libs/* /data-transfer/artefacts/libs
+
+mkdir -p /data-transfer/artefacts/config
+cp -R /config/* /data-transfer/artefacts/config
+
 
